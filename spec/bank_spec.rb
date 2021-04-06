215236f5c0916d2214d51f04514e645a8f152a39
Eds <steadyede@live.co.uk>
2021-04-06 12:15:13 +0100

    test 2, initial balance

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,9 +1,14 @@
 require 'bank'
 
 describe Bank do
-  it 'should respond to a balance method' do
-    expect(subject).to respond_to(:balance)
-    # expect(subject.balance).to eq 0
+  describe '.balance' do
+    it 'should respond to a balance method' do
+      expect(subject).to respond_to(:balance)
+    end
+    it 'should have initial value of 0' do
+      expect(subject.balance).to eq 0
+    end
+
   end
 
 end
